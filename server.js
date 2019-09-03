20c75a06f6cfedc5836023f170f50074c06474ea
Marques Johnson <marques.johnson1992@gmail.com>
2019-09-03 18:55:13 -0400

    mvp and one stretch

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const server = express();
 const router = require("./router");
 
-server.use(expressjson());
+server.use(express.json());
 server.use("/api/posts", router);
 
 server.get("/", (req, res) => {
-    res.send("One ring to rule them all");
+  res.send("One Ring to Rule them All");
 });
 
 module.exports = server;
